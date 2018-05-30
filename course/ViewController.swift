1b668416e820c65c3317ee548e01d401b1f6f251
Sergey <s.s.r@mail.ru>
2018-05-30 16:51:46 +0300

    1-7

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -1,26 +1,19 @@
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
+        let button = UIButton()
+        button.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        button.backgroundColor = .orange
+        view.addSubview(button)
         
-        let button1 = UIButton()
-        button1.bounds =  CGRect(x: 0, y: 0, width: 100, height: 100)
-        button1.center = view.center
-        button1.setTitle("button", for: .normal)
-        button1.backgroundColor = .orange
-        button1.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
         
-        view.addSubview(button1)
-    }
 
+        
 
-    @objc func buttonPressed(sender: UIButton){
-    sender.backgroundColor = UIColor.black
     }
 
 }
