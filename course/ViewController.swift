e3aa62b2517471595a70d55a3242588bbd2b86ca
Sergey <s.s.r@mail.ru>
2018-05-31 18:34:32 +0300

    some experiments with frames and bounds

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -14,6 +14,35 @@ class ViewController: UIViewController {
         button.setTitle("button", for: .normal)
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
+        let control = UIControl()
+        
+     //   control.center = CGPoint(x: 200, y: 200)
+        control.frame = CGRect(x: 50, y: 50, width: 200, height: 200)
+       // control.getGradientBackground(colorOne: .red, colorTwo: .blue)
+        
+//        control.bounds.origin = CGPoint(x: 50, y: 100)
+        
+        control.transform = CGAffineTransform(rotationAngle: -CGFloat((Double.pi / 4)))
+        
+        control.bounds.origin = CGPoint(x: 50, y: 100)
+
+        // = CGRect(x: 100, y: 100, width: 100, height: 100)
+//        control.bounds.size = CGSize(width: 100, height: 50)
+//        control.bounds.origin = CGPoint(x: 100, y: 100)
+//       print("control.bounds.origin.x")
+//       print(control.bounds.origin.x)
+//        print("control.bounds.origin.y")
+//        print(control.bounds.origin.y)
+//        //control.center = view.center
+//
+//        control.frame.origin = CGPoint(x: 100, y: 0)
+        
+        
+        view.addSubview(control)
+        control.backgroundColor = .green
+        control.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        
+        
 
     }
     @objc func buttonPressed(){
@@ -21,4 +50,21 @@ class ViewController: UIViewController {
     }
 
 }
+
+extension UIView {
+    
+    func getGradientBackground(colorOne: UIColor, colorTwo: UIColor){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.6, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+        
+        
+    }
+}
+
 
