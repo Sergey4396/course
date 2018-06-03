39fc215cee45d80d4632d5b585fce8efa37eb341
Sergey <s.s.r@mail.ru>
2018-06-03 11:11:11 +0300

    35-38

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -5,13 +5,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let button = UIButton(type: .system)
-        button.bounds = CGRect(x: 0, y: 0, width: 200, height: 80)
-        button.backgroundColor = .orange
+        let button = UIButton(type: .detailDisclosure)
+//        button.bounds = CGRect(x: 0, y: 0, width: 200, height: 80)
+//        button.backgroundColor = .orange
         view.addSubview(button)
         
         button.center = view.center
-        button.setTitle("button", for: .normal)
+ //       button.setTitle("button", for: .normal)
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
 
