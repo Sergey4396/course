26b1e87b504677e2bddae7d029db16d803da3925
Sergey <s.s.r@mail.ru>
2018-06-05 09:38:32 +0300

    88-98

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -12,35 +12,26 @@ class ViewController: UIViewController {
         view.addSubview(button)
         
         button.center = view.center
-        button.setTitle("button", for: .normal)
+        button.setTitle("button very long button", for: .normal)
         button.setTitle("pressed", for: .highlighted)
         button.setTitle("disabled", for: .disabled)
 
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
+        button.titleLabel?.text = "New text"
+        button.titleLabel?.textColor = .red
         
-        let secondButton = UIButton(frame: CGRect(x: view.bounds.width / 2, y: view.bounds.height * 0.75, width: 200, height: 80))
-        view.addSubview(secondButton)
-        secondButton.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height * 0.75)
-        secondButton.addTarget(self, action: #selector(secondButtonPressed), for: .touchUpInside)
-        secondButton.setTitle("second", for: .normal)
-        secondButton.backgroundColor = .red
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.titleLabel?.numberOfLines = 2
+
     }
     @objc func buttonPressed(sender: UIButton){
         print("button pressed")
-        sender.isEnabled = false
+
         
-        print("button.title(for: .normal)")
-        print(button.title(for: .normal))
-        print("button.title(for: .highlighted)")
-        print(button.title(for: .highlighted))
-        print("button.title(for: .disabled)")
-        print(button.title(for: .disabled))
-        
+
     }
-    @objc func secondButtonPressed(){
-        button.isEnabled = true
-    }
+
 
 }
 
