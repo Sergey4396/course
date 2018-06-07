ffccdbba4f092f13872069abf525856783726bd2
Sergey <s.s.r@mail.ru>
2018-06-07 16:59:52 +0300

    130 some experiments with images and edges

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -11,18 +11,44 @@ class ViewController: UIViewController {
         view.addSubview(button)
         button.center = view.center
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        button.setTitle("button", for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-        button.setTitleColor(.yellow, for: .normal)
+        let img = UIImage(named: "swiftImg")
         
-        button.setTitleShadowColor(.gray, for: .normal)
-        button.titleLabel?.shadowOffset = CGSize(width: 3, height: 3)
-        button.reversesTitleShadowWhenHighlighted = true
 
 
+        
+        button.setTitle("Swift", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.titleEdgeInsets = UIEdgeInsetsMake(10, button.bounds.width * 0.35, 30, button.bounds.width * 0.05)
+        
+        
+        let imgPressed = UIImage(named: "imgButtonActive")
+        button.setImage(img, for: .selected)
+        
+        button.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsetsMake(0, button.bounds.width * 0.05, 0, button.bounds.width * 0.65)
+        button.setTitle("Swift", for: .highlighted)
+        button.isSelected = true
+        
+        button.setTitle("selected", for: .selected)
+        
+//        button.setBackgroundImage(img, for: .normal)
+//        button.isEnabled = false
+        
+        button.adjustsImageWhenHighlighted = true
+        button.adjustsImageWhenDisabled = true
+        button.showsTouchWhenHighlighted = true
+        
+        button.imageView?.isHighlighted = true
+
+    //    button.imageView?.leftAnchor.constraint(equalTo: button.leftAnchor).isActive = true
+
+
+        
     }
     @objc func buttonPressed(sender: UIButton){
         print("button pressed")
+        sender.isSelected = !sender.isSelected
     }
 }
 
