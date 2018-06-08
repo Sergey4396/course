bba7c73563804f952b233f8ef4fdd50dc421eb80
Sergey <s.s.r@mail.ru>
2018-06-08 11:03:25 +0300

    unsuccessful experiments with a decreasing button.

--- course/ViewController.swift
+++ course/ViewController.swift
@@ -1,54 +1,136 @@
 import UIKit
-
 class ViewController: UIViewController {
-    var button = UIButton()
+    var button = MyShrinkingButton()
+    var button2 = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        button = UIButton()
+        button = MyShrinkingButton()
         button.bounds = CGRect(x: 0, y: 0, width: 200, height: 80)
         button.backgroundColor = .orange
-        view.addSubview(button)
         button.center = view.center
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        let img = UIImage(named: "swiftImg")
+        view.addSubview(button)
         
-
-
+//        let img = UIImage(named: "imgNew")
+//        button.setImage(img, for: .normal)
         
-        button.setTitle("Swift", for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-        button.titleLabel?.adjustsFontSizeToFitWidth = true
-        button.titleEdgeInsets = UIEdgeInsetsMake(10, button.bounds.width * 0.35, 30, button.bounds.width * 0.05)
-        
-        
-        let imgPressed = UIImage(named: "imgButtonActive")
-        button.setImage(img, for: .selected)
-        
-        button.imageView?.contentMode = UIViewContentMode.scaleAspectFit
-        button.imageEdgeInsets = UIEdgeInsetsMake(0, button.bounds.width * 0.05, 0, button.bounds.width * 0.65)
-        button.setTitle("Swift", for: .highlighted)
-        button.isSelected = true
-        
-        button.setTitle("selected", for: .selected)
-        
-//        button.setBackgroundImage(img, for: .normal)
-//        button.isEnabled = false
-        
-        button.adjustsImageWhenHighlighted = true
-        button.adjustsImageWhenDisabled = true
-        button.showsTouchWhenHighlighted = true
-        
-        button.imageView?.isHighlighted = true
-
-    //    button.imageView?.leftAnchor.constraint(equalTo: button.leftAnchor).isActive = true
-
-
+//
+//        button2 = UIButton()
+//        button2.bounds = CGRect(x: 0, y: 0, width: 200, height: 80)
+//        button2.backgroundColor = .orange
+//        button2.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height * 0.75)
+//        button2.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+//        view.addSubview(button2)
+//        let img2 = UIImage(named: "imgNew")
+//        button2.setBackgroundImage(img2, for: .normal)
+//
+//        button.setTitle("Title22", for: .normal)
+//        button2.setTitle("Title22", for: .normal)
+//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 50)
+//        button2.titleLabel?.font = UIFont.boldSystemFont(ofSize: 50)
+//        button.setTitleColor(.black, for: .normal)
+//        button2.setTitleColor(.black, for: .normal)
         
     }
     @objc func buttonPressed(sender: UIButton){
         print("button pressed")
-        sender.isSelected = !sender.isSelected
+
     }
 }
+
+
+extension CGRect {
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        self.init(x:x, y:y, width:w, height:h)
+    }
+}
+extension CGSize {
+    init(_ width:CGFloat, _ height:CGFloat) {
+        self.init(width:width, height:height)
+    }
+}
+extension CGPoint {
+    init(_ x:CGFloat, _ y:CGFloat) {
+        self.init(x:x, y:y)
+    }
+}
+extension CGVector {
+    init (_ dx:CGFloat, _ dy:CGFloat) {
+        self.init(dx:dx, dy:dy)
+    }
+}
+
+
+
+extension CGSize {
+    func sizeByDelta(dw:CGFloat, dh:CGFloat) -> CGSize {
+        return CGSize(self.width + dw, self.height + dh)
+    }
+}
+
+class MyShrinkingButton: UIButton {
+    
+    override func backgroundRect(forBounds bounds: CGRect) -> CGRect {
+        var result = super.backgroundRect(forBounds:bounds)
+        if self.isHighlighted {
+            result = result.insetBy(dx: 3, dy: 3)
+        }
+        return result
+    }
+    
+    override var intrinsicContentSize : CGSize {
+        return super.intrinsicContentSize.sizeByDelta(dw:25, dh: 20)
+    }
+    
+    
+}
+
+
+
+
+
+//extension CGSize {
+//    func sizeByDelta(dw:CGFloat, dh:CGFloat) -> CGSize {
+//        return CGSize(width:self.width + dw, height:self.height + dh)
+//    }
+//}
+//class MyShrinkingButton: UIButton {
+//    override func backgroundRect(forBounds bounds: CGRect) -> CGRect {
+//        var result = super.backgroundRect(forBounds:bounds)
+//        if self.isHighlighted {
+//            result = result.insetBy(dx: 3, dy: 3)
+//        }
+//        return result
+//    }
+//    override var intrinsicContentSize : CGSize {
+//        return super.intrinsicContentSize.sizeByDelta(dw:25, dh: 20)
+//    }
+//}
+
+
+
+
+
+
+
+
+
+//extension CGSize {
+//    func sizeByDelta(dw:CGFloat, dh:CGFloat) -> CGSize {
+//        return CGSize(self.width + dw, self.height + dh)
+//    }
+//}
+//class MyShrinkingButton: UIButton {
+//    override func backgroundRect(forBounds bounds: CGRect) -> CGRect {
+//        var result = super.backgroundRect(forBounds:bounds)
+//        if self.isHighlighted {
+//            result = result.insetBy(dx: 3, dy: 3)
+//        }
+//        return result
+//    }
+//    override var intrinsicContentSize : CGSize {
+//        return super.intrinsicContentSize.sizeByDelta(dw:25, dh: 20)
+//    }
+//}
 
